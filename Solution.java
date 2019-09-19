4481e1a28025ffb00edd54aa522505a5b77d3d09
Lachezar Kutsarov <lkutsarov@gmail.com>
2019-09-19 14:26:36 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -40,7 +40,7 @@ public class Solution {
     * The array stores the possibilty to create each sum, up to the target value. 
     * If current sum (represented by the value of "i") can be created with
     * the available coin values, then possibleValues[i] = true. 
-    * Othewise possibleValues[i] = false.
+    * Othewise, possibleValues[i] = false.
     */
     boolean[] possibleValues = new boolean[targetValue + 1];
     possibleValues[0] = true;
